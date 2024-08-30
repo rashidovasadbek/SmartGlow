0cf7dc92d55429669b4ea12bd462767f32f08c2b
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-08-30 23:16:29 +0500

    Add AppDbContext and EfCoreExtensions

---
+++ src/SmartGlow.Backend/SmartGlow.Persistence/DataContext/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartGlow.Domain.Entities;
+
+namespace SmartGlow.Persistence.DataContext;
+
+public class AppDbContext(DbContextOptions<AppDbContext> dbContextOptions): DbContext(dbContextOptions)
+{
+    #region Identity Infrastructure
+    
+    public DbSet<User> Users => Set<User>();
+    
+    #endregion
+}
---
+++ src/SmartGlow.Backend/SmartGlow.Persistence/Extensions/EfCoreExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartGlow.Domain.Common.Queries;
+
+namespace SmartGlow.Persistence.Extensions;
+
+public static class EfCoreExtensions
+{
+    /// <summary>
+    /// Applies pagination to given query source
+    /// </summary>
+    /// <param name="source">Queryable source</param>
+    /// <param name="trackingMode">Tracking mode to apply</param>
+    /// <typeparam name="TSource">Query source type</typeparam>
+    /// <returns>Query source with pagination applied</returns>
+    public static IQueryable<TSource> ApplyTrackingMode<TSource>(this IQueryable<TSource> source,
+        QueryTrackingMode trackingMode) where TSource: class
+    {
+        return trackingMode switch
+        {
+            QueryTrackingMode.AsTracking => source,
+            QueryTrackingMode.AsNoTracking => source.AsNoTracking(),
+            QueryTrackingMode.AsNoTrackingWithIdentityResolution => source.AsNoTrackingWithIdentityResolution(),
+            _ => source
+        };
+    }
+}
