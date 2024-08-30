30b2a7128ba005f664150e4a35d07acc57434d65
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-08-30 23:14:23 +0500

    Add base entities

---
+++ src/SmartGlow.Backend/SmartGlow.Domain/Common/Entities/IEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace SmartGlow.Domain.Common.Entities;
+
+/// <summary>
+/// Defines an entity within the system
+/// </summary>
+public interface IEntity
+{
+    /// <summary>
+    /// Gets or sets the unique identifier for the entity. This identifier should be unique globally unique within the system
+    /// </summary>
+    public Guid Id { get; set; }
+}
---
+++ src/SmartGlow.Backend/SmartGlow.Domain/Common/Entities/ISoftDeletedEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace SmartGlow.Domain.Common.Entities;
+
+/// <summary>
+/// Represents an entity that supports soft deletion, inheriting properties from IEntity
+/// </summary>
+public interface ISoftDeletedEntity : IEntity
+{
+    /// <summary>
+    ///Gets or sets a value indicating whether the entity is soft deleted
+    /// </summary>
+    public bool IsDeleted { get; set; }
+    
+    /// <summary>
+    /// Get or sets date and time when the entity was soft deleted. This value will be null if the entity had not been soft deleted
+    /// </summary>
+    public DateTimeOffset? DeletedTime { get; set; }  
+}
