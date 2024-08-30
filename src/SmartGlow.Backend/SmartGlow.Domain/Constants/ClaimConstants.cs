6b89080b427315469561f2a00eafbb6a5f1e6749
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-08-30 23:13:42 +0500

    Add Constants

---
+++ src/SmartGlow.Backend/SmartGlow.Domain/Constants/ClaimConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace SmartGlow.Domain.Constants;
+
+/// <summary>
+/// Contains constants for standard claim names.
+/// </summary>
+public static class ClaimConstants
+{
+    /// <summary>
+    /// Constant representing the User ID claim name.
+    /// </summary>
+    public const string UserId = "UserId";
+}
---
+++ src/SmartGlow.Backend/SmartGlow.Domain/Constants/DataAccessConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace SmartGlow.Domain.Constants;
+
+/// <summary>
+/// Represents Data Access Constants
+/// </summary>
+public static class DataAccessConstants
+{
+    /// <summary>
+    /// Database connection string constant
+    /// </summary>
+    public const string DbConnectionString = "DbConnectionString";
+}   
