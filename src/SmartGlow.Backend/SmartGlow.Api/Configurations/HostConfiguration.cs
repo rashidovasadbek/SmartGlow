7a8ae9f8e5dcc6306d312306820b4aa951923f39
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-08-31 17:00:04 +0500

    Add HostConfiguration and connected to database

---
+++ src/SmartGlow.Backend/SmartGlow.Api/Configurations/HostConfiguration.cs
@@ -0,0 +1,23 @@
+﻿namespace SmartGlow.Api.Configurations;
+
+public static partial class HostConfiguration
+{
+    public static ValueTask<WebApplicationBuilder> ConfigureAsync(this WebApplicationBuilder builder)
+    {
+        builder
+            .AddPersistence()
+            .AddDevTools()
+            .AddExposers();
+
+        return new(builder);
+    }
+
+    public static async ValueTask<WebApplication> ConfigureAsync(this WebApplication app)
+    {
+        app
+            .UseExposers()
+            .UseDevTools();
+            
+        return app;
+    }
+}
