9a0c5ccb12ac2e542a81d9c4a1274b1fb79dd0c8
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-08-31 17:00:59 +0500

    Add validation files

---
+++ src/SmartGlow.Backend/SmartGlow.Infrastructure/Common/Validators/UserValidator.cs
@@ -0,0 +1,49 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Options;
+using SmartGlow.Domain.Entities;
+using SmartGlow.Domain.Enums;
+using SmartGlow.Infrastructure.Settings;
+
+namespace SmartGlow.Infrastructure.Common.Validators;
+
+public class UserValidator : AbstractValidator<User> 
+{
+    public UserValidator(IOptions<ValidationSettings> validationSettings)
+    {
+        var settings = validationSettings.Value;
+        
+        RuleSet(
+            IdentityEvent.OnSignUpUser.ToString(),
+            () =>
+            {
+                RuleFor(user => user.FirstName)
+                    .NotEmpty()
+                    .MinimumLength(3)
+                    .MaximumLength(128)
+                    .Matches(settings.PersonNameRegexPattern);
+                
+                RuleFor(user => user.LastName)
+                    .NotEmpty()
+                    .MinimumLength(3)
+                    .MaximumLength(128)
+                    .Matches(settings.PersonNameRegexPattern);
+                
+                RuleFor(user => user.UserName)
+                    .NotEmpty()
+                    .MinimumLength(3)
+                    .MaximumLength(128)
+                    .Matches(settings.PersonNameRegexPattern);
+                
+                RuleFor(user => user.PhoneNumber)
+                    .NotEmpty()
+                    .MinimumLength(13)
+                    .MaximumLength(16)
+                    .Matches(settings.PhoneNumberRegexPattern);
+                
+                RuleFor(user => user.Password)
+                    .NotEmpty()
+                    .MinimumLength(8)
+                    .Matches(settings.PasswordRegexPattern);
+            });
+    }
+}
