bcc027794ff97960c4822bfa99abcc22ce0af57a
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-09-12 16:42:52 +0500

    Added passwordhasher serivce and password genetare service

---
+++ src/SmartGlow.Backend/SmartGlow.Infrastructure/Common/Identity/Services/PasswordHasherService.cs
@@ -0,0 +1,17 @@
+﻿using BC = BCrypt.Net.BCrypt;
+using SmartGlow.Application.Common.Identity.Services;
+
+namespace SmartGlow.Infrastructure.Common.Identity.Services;
+
+public class PasswordHasherService : IPasswordHasherService
+{
+    public string HashPassword(string password)
+    {
+        return BC.HashPassword(password);
+    }
+
+    public bool ValidatePassword(string password, string hashPassword)
+    {
+        return BC.Verify(password, hashPassword);
+    }
+}
