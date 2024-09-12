0f3f6d03a145b058f7628e5bf1d9d28c52d8fead
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-09-12 17:01:51 +0500

    Added Identity User Mediatr Validator functions in HostConfiguration and added seedData

--- src/SmartGlow.Backend/SmartGlow.Api/Configurations/HostConfiguration.Extensions.cs
+++ src/SmartGlow.Backend/SmartGlow.Api/Configurations/HostConfiguration.Extensions.cs
@@ -1,9 +1,28 @@
 ﻿using System.Reflection;
+using System.Text;
 using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using SmartGlow.Api.Data;
+using SmartGlow.Api.Middlewares;
+using SmartGlow.Application.Common.Identity.Services;
+using SmartGlow.Application.Settings;
+using SmartGlow.Application.Users.Services;
+using SmartGlow.Domain.Brokers;
 using SmartGlow.Domain.Constants;
+using SmartGlow.Domain.Entities;
+using SmartGlow.Infrastructure.Common.Caching;
+using SmartGlow.Infrastructure.Common.Identity.Services;
+using SmartGlow.Infrastructure.Common.Identity.Validators;
+using SmartGlow.Infrastructure.Common.RequestContext.Brokers;
 using SmartGlow.Infrastructure.Settings;
+using SmartGlow.Infrastructure.Users.Services;
+using SmartGlow.Persistence.Caching.Brokers;
 using SmartGlow.Persistence.DataContext;
+using SmartGlow.Persistence.Repositories;
+using SmartGlow.Persistence.Repositories.Interfaces;
 
 namespace SmartGlow.Api.Configurations;
 
@@ -16,6 +35,41 @@ public static partial class HostConfiguration
         .Append(Assembly.GetExecutingAssembly())
         .ToList();
 
+    private static WebApplicationBuilder AddCaching(this WebApplicationBuilder builder)
+    {
+        // Configure CacheSettings from the app settings.
+        builder.Services.Configure<CacheSettings>(builder.Configuration.GetSection(nameof(CacheSettings)));
+
+        // Register the Memory Cache service.
+        builder.Services.AddLazyCache();
+        
+        // Register the Memory Cache as a singleton.
+        builder.Services.AddSingleton<ICacheBroker, LazyMemoryCacheBroker>();
+        
+        // Register middlewares
+        builder.Services.AddSingleton<AccessTokenValidationMiddleware>();
+
+        return builder;
+    }
+    
+    private static WebApplicationBuilder AddClientInfrastructure(this WebApplicationBuilder builder)
+    {
+        // Register repositories
+        builder.Services
+            .AddScoped<IUserRepository, UserRepository>();
+        
+        // Register services
+        builder.Services
+            .AddScoped<IUserService, UserService>();
+
+        return builder;
+    }
+    
+    private static WebApplicationBuilder AddMappers(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddAutoMapper(Assemblies);
+        return builder;
+    }
     
     /// <summary>
     /// Registers persistence infrastructure
@@ -36,14 +90,125 @@ public static partial class HostConfiguration
     }
 
     /// <summary>
+    /// Configures devTools including controllers
+    /// Configures IdentityInfrastructure including controllers
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <returns></returns>
+     private static WebApplicationBuilder AddIdentityInfrastructure(this WebApplicationBuilder builder)
+    {
+        //configuration settings
+        builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection(nameof(JwtSettings)));
+        builder.Services.Configure<PasswordValidationSettings>(builder.Configuration.GetSection(nameof(PasswordValidationSettings)));
+
+        //register repository
+        builder.Services
+            .AddScoped<IAccessTokenRepository, AccessTokenRepository>()
+            .AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+            
+        //register services
+        builder.Services
+            .AddScoped<IAccountService, AccountService>()
+            .AddScoped<IAuthService, AuthService>()
+            .AddScoped<IIdentitySecurityTokenGeneratorService, IdentitySecurityTokenGeneratorService>()
+            .AddScoped<IIdentitySecurityTokenService, IdentitySecurityTokenService>()
+            .AddScoped<IPasswordGeneratorService, PasswordGeneratorService>()
+            .AddScoped<IPasswordHasherService, PasswordHasherService>();
+        
+        
+        
+        // register authentication handlers
+        var jwtSettings = builder.Configuration.GetSection(nameof(JwtSettings)).Get<JwtSettings>() ??
+                          throw new InvalidOperationException("JwtSettings is not configured.");
+        
+        // add authentication
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(
+                options =>
+                {
+                    options.RequireHttpsMetadata = false;
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = jwtSettings.ValidateIssuer,
+                        ValidIssuer = jwtSettings.ValidIssuer,
+                        ValidAudience = jwtSettings.ValidAudience,
+                        ValidateAudience = jwtSettings.ValidateAudience,
+                        ValidateLifetime = jwtSettings.ValidateLifetime,
+                        ValidateIssuerSigningKey = jwtSettings.ValidateIssuerSigningKey,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecretKey))
+                    };
+                }
+            );
+        
+        return builder;
+    }
+    
+    /// <summary>
     /// Configures the Dependency Injection container to include validators from referenced assemblies.
     /// </summary>
     private static WebApplicationBuilder AddValidators(this WebApplicationBuilder builder)
     {
         builder.Services.Configure<ValidationSettings>(builder.Configuration.GetSection(nameof(ValidationSettings)));
-        builder.Services.AddValidatorsFromAssemblies(Assemblies);
+        builder.Services.AddValidatorsFromAssemblies(Assemblies.ToArray()).AddFluentValidationAutoValidation();
+
+        builder.Services.AddTransient<IValidator<RefreshToken>, RefreshTokenValidator>();
+        
+        return builder;
+    }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <returns></returns>
+    
+    private static WebApplicationBuilder AddMediator(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddMediatR(conf => {conf.RegisterServicesFromAssemblies(Assemblies.ToArray());});
+        
+        return builder;
+    }
+    
+    /// <summary>
+    /// Configures Request Context tool for the web application.
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <returns></returns>
+    private static WebApplicationBuilder AddRequestContextTools(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddHttpContextAccessor();
+
+        builder.Services.AddScoped<IRequestUserContextProvider, RequestUserContextProvider>();
 
         return builder;
+    }
+    
+    /// <summary>
+    /// Asynchronously migrates database schemas associated with the application.
+    /// </summary>
+    /// <param name="app">The WebApplication instance to configure.</param>
+    /// <returns>A ValueTask representing the asynchronous operation, with the WebApplication instance.</returns>
+    private static async ValueTask<WebApplication> MigrateDataBaseSchemasAsync(this WebApplication app)
+    {
+        var serviceScopeFactory = app.Services.GetRequiredKeyedService<IServiceScopeFactory>(null);
+        
+        await serviceScopeFactory.MigrateAsync<AppDbContext>();
+        
+        return app;
+    }
+    
+    /// <summary>
+    /// Seeds data into the application's database by creating a service scope and initializing the seed operation.
+    /// </summary>
+    /// <param name="app"></param>
+    /// <returns></returns>
+    private static async ValueTask<WebApplication> SeedDataAsync(this WebApplication app)
+    {
+        var serviceScope = app.Services.CreateScope();
+        await serviceScope.ServiceProvider.InitializeSeedAsync();
+
+        return app;
     }
     
     /// <summary>
@@ -51,6 +216,7 @@ public static partial class HostConfiguration
     /// </summary>
     private static WebApplicationBuilder AddDevTools(this WebApplicationBuilder builder)
     {
+        builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
         
         return builder;
