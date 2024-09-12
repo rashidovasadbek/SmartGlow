db78999a5dd63c0033463569be6202a52490da28
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-09-12 16:42:07 +0500

    Adede AccesToken dto mapper validate

---
+++ src/SmartGlow.Backend/SmartGlow.Infrastructure/Common/Identity/Validators/CredentialDetailsValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using SmartGlow.Application.Common.Identity.Models;
+
+namespace SmartGlow.Infrastructure.Common.Identity.Validators;
+
+/// <summary>
+/// Validator for <see cref="CredentialDetails"/> objects.
+/// </summary>
+public class CredentialDetailsValidator : AbstractValidator<CredentialDetails>
+{
+    public CredentialDetailsValidator()
+    {
+        RuleFor(credential => credential.Password)
+            .Custom(
+                (password, context) =>
+                {
+                    if (context.RootContextData.TryGetValue("PersonalInformation", out var userInfoObj) &&
+                        userInfoObj is IEnumerable<string> userInfo &&
+                        userInfo.Any(info => !string.IsNullOrEmpty(info) && password.Contains(info)))
+                        context.AddFailure("Password must not contain user public information.");
+                }
+            );
+    }
+}
