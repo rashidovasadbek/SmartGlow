98f67152bb25ef029cdc4d1e126284c3b6c7a66f
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-09-12 16:32:38 +0500

    Added UserConfiguration and usermigration

---
+++ src/SmartGlow.Backend/SmartGlow.Persistence/EntityConfiguration/UserConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmartGlow.Domain.Entities;
+using SmartGlow.Domain.Enums;
+
+namespace SmartGlow.Persistence.EntityConfiguration;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.Property(client => client.FirstName).HasMaxLength(64).IsRequired();
+        builder.Property(client => client.LastName).HasMaxLength(64).IsRequired();
+        builder.Property(client => client.UserName).HasMaxLength(128).IsRequired();
+        builder.Property(client => client.PasswordHash).HasMaxLength(128).IsRequired();
+        
+        
+        builder
+            .Property(client => client.Role)
+            .HasConversion<string>()
+            .HasDefaultValue(RoleType.User)
+            .HasMaxLength(64)
+            .IsRequired();
+    }
+}
---
+++ src/SmartGlow.Backend/SmartGlow.Persistence/Migrations/20240910163447_Add_Users.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartGlow.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Users : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: false),
+                    AttachedStreets = table.Column<string[]>(type: "text[]", nullable: false),
+                    Role = table.Column<byte>(type: "smallint", nullable: false),
+                    UserName = table.Column<string>(type: "text", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    DeletedTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    CreatedTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    ModifiedTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
