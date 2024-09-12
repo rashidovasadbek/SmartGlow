3e7163544ff130d6606f720c0478566889057c6c
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-09-12 16:48:26 +0500

    Adedd signInDetails and SignUpDetails and SIgnInCommand and SignOutCommand

---
+++ src/SmartGlow.Backend/SmartGlow.Application/Common/Identity/Commands/SignOutCommand.cs
@@ -0,0 +1,11 @@
+﻿using SmartGlow.Domain.Common.Commands;
+
+namespace SmartGlow.Application.Common.Identity.Commands;
+
+public class SignOutCommand : ICommand<bool>
+{
+    /// <summary>
+    /// User's refresh token
+    /// </summary>
+    public string RefreshToken { get; set; } = default!;
+}
---
+++ src/SmartGlow.Backend/SmartGlow.Application/Common/Identity/Models/SignUpDetails.cs
@@ -0,0 +1,34 @@
+﻿namespace SmartGlow.Application.Common.Identity.Models;
+
+public class SignUpDetails
+{
+    /// <summary>
+    /// Gets or sets first name of the sign up details
+    /// </summary>
+    public string FirstName { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets last name of the sign up details
+    /// </summary>
+    public string LastName { get; set; } = default!;
+    
+    /// <summary>
+    /// Gets or sets username of the sign up details
+    /// </summary>
+    public string UserName { get; set; } = default!;
+    
+    /// <summary>
+    /// Gets or sets attachedStreets of the sign up details
+    /// </summary>
+    public IList<string> AttachedStreets { get; set; } = [];
+   
+    /// <summary>
+    /// PhoneNumber of the sign up details
+    /// </summary>
+    public string PhoneNumber { get; set; } = default!;
+    
+    /// <summary>
+    /// Password of the sign up details
+    /// </summary>
+    public string Password { get; set; } = default!;
+}
