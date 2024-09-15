16575c190cc9b49bd643133bcfdfc30b00145581
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-09-15 16:47:03 +0500

    Aded OnOffTiming entity and their Repository

---
+++ src/SmartGlow.Backend/SmartGlow.Domain/Entities/OnOffTiming.cs
@@ -0,0 +1,41 @@
+﻿using SmartGlow.Domain.Common.Entities;
+
+namespace SmartGlow.Domain.Entities;
+
+public class OnOffTiming : AuditableEntity
+{
+    /// <summary>
+    /// Gets or sets the onTime of the Lumps
+    /// </summary>
+    public DateTime OnTime { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the OffTime of the Lumps
+    /// </summary>
+    public DateTime OffTime { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the OnOffTime of the Lumps
+    /// </summary>
+    public byte OnLights { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the OnOffTime of the Lumps
+    /// </summary>
+    public byte OffLights { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the LitUnits of the Lumps
+    /// </summary>
+    public byte LitUnits { get; set; }
+    
+    /// <summary>
+    /// The unique identifier of the street associated with this OnOffTiming.
+    /// </summary>
+    public Guid StreetId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the Street
+    /// </summary>
+    public Street Street { get; set; } = default!;
+}
---
+++ src/SmartGlow.Backend/SmartGlow.Persistence/Repositories/OnOffTimingRepository.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq.Expressions;
+using SmartGlow.Domain.Common.Commands;
+using SmartGlow.Domain.Common.Queries;
+using SmartGlow.Domain.Entities;
+using SmartGlow.Persistence.Caching.Brokers;
+using SmartGlow.Persistence.DataContext;
+using SmartGlow.Persistence.Repositories.Interfaces;
+
+namespace SmartGlow.Persistence.Repositories;
+
+public class OnOffTimingRepository(AppDbContext appDbContext, ICacheBroker cacheBroker) : EntityRepositoryBase<OnOffTiming, AppDbContext>(appDbContext, cacheBroker), IOnOffTimingRepository
+{
+    public new IQueryable<OnOffTiming> Get(Expression<Func<OnOffTiming, bool>>? predicate = default, QueryOptions queryOptions = default)
+    {
+        return base.Get(predicate, queryOptions);
+    }
+
+    public new ValueTask<OnOffTiming?> GetByIdAsync(Guid onOfTimingId, QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default)
+    {
+        return base.GetByIdAsync(onOfTimingId, queryOptions, cancellationToken);
+    }
+
+    public new ValueTask<bool> CheckAsync<TValue>(IQueryable<TValue> queryableSource, TValue? expectedValue = default,
+        CancellationToken cancellationToken = default)
+    {
+        return base.CheckAsync<TValue>(queryableSource, expectedValue);
+    }
+
+    public new ValueTask<OnOffTiming> CreateAsync(OnOffTiming onOffTiming, CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default)
+    {
+        return base.CreateAsync(onOffTiming, commandOptions, cancellationToken);
+    }
+
+    public ValueTask<OnOffTiming> UpdateAsync(OnOffTiming onOffTiming, CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default)
+    {
+        return base.UpdateAsync(onOffTiming, commandOptions, cancellationToken);
+    }
+
+    public new ValueTask<OnOffTiming?> DeleteByIdAsync(Guid onOffTimingId, CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default)
+    {
+        return base.DeleteByIdAsync(onOffTimingId, commandOptions, cancellationToken);
+    }
+
+    public new ValueTask<OnOffTiming?> DeleteAsync(OnOffTiming onOffTiming, CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default)
+    {
+        return base.DeleteAsync(onOffTiming, commandOptions, cancellationToken);
+    }
+}
